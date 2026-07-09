3bbf41dfd09bc9f6d78f590d419ed365edebefd6
Harshiizz456 <harshitashah236@gmail.com>
2026-07-10 02:03:45 +0530

    Update palindrome.py

--- Strings/Strings/palindrome.py
+++ Strings/Strings/palindrome.py
@@ -1,1 +1,22 @@
-num = input("Enter a string: ") rev_num = num[::-1] if rev_num == num: print("palindrome") else: print("not a palindrome")
+# Approach 1: Basic
+
+num = input("Enter a string: ")
+
+rev_num = num[::-1]
+
+if rev_num == num:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
+
+
+# Approach 2: Optimized (ignore spaces & case)
+
+s = input("Enter a string: ")
+
+cleaned = s.replace(" ", "").lower()
+
+if cleaned == cleaned[::-1]:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
